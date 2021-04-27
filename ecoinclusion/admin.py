a45fd44451e5a4c63f7b6aa38ae79d4216aea529
cettipao <cettipao@live.com.ar>
2021-04-27 10:12:06 -0300

    Problema con MultipleSelectForm pendiente

--- ecoinclusion/admin.py
+++ ecoinclusion/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+from django.forms import CheckboxSelectMultiple
+
+# Overriding standart CheckboxSelectMultiple for jet admin
+class JetCheckboxSelectMultiple(CheckboxSelectMultiple):
+    option_template_name = 'admin/forms/widgets/checkbox_option.html'
+
 # Register your models here.
+@admin.register(PuntoDeAcopio)
+class PuntoDeAcopioAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        MultiSelectField: {'widget': JetCheckboxSelectMultiple},
+    }
 
 admin.site.register(Contribuyente)
 admin.site.register(Intermediario)
 admin.site.register(CentroDeReciclaje)
+
--- ecoinclusion/models.py
+++ ecoinclusion/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from phone_field import PhoneField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -31,6 +32,21 @@ class CentroDeReciclaje(models.Model):
     nombre = models.CharField(max_length=100)
     celular = PhoneField(blank=True, help_text='Celular')
 
+    def __str__(self):
+        return self.usuario.username
+
+materiales =(
+    ("1", "Secos"),
+    ("2", "Humedos"),
+    ("3", "Alumnio"),
+)
+class PuntoDeAcopio(models.Model):
+    coordenadas = models.CharField(max_length=100)
+    tipoDeReciclado = MultiSelectField(choices=materiales, max_choices=3, max_length=1)
+    centro = models.ForeignKey(
+        'CentroDeReciclaje',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.usuario.username
