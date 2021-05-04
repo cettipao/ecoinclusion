9d8948cc85abd0af8d62e225a5ee0c0cf98b374f
Juan Martin <juanmanuelmartinrodrigo@gmail.com>
2021-05-04 17:15:38 -0300

    Templates Login y Register, requirements updated

--- ecoinclusion/views.py
+++ ecoinclusion/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def logoutViews(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect("login_url")
+
+def registerView(request):
+    return render(request, 'register.html')
+
+def loginView(request):
+    return render(request,'login.html')
