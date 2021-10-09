ee5a7216c085c0273fccdbc3a005363d3ce8ae5a
Federico Sironi <sironifede@gmail.com>
2021-10-09 19:21:02 -0300

    debug true

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1o0qkjv22ft@@3-!1gsije%_^8utwpxk7v^bb$cb832p_98i24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
