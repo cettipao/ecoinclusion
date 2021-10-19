b24de23b611d1654766c33b40a3ca9e2206a09df
Federico Sironi <sironifede@gmail.com>
2021-10-19 10:44:27 -0300

    correccion en la cooperativa

--- ecoinclusion/models.py
+++ ecoinclusion/models.py
@@ -33,7 +33,7 @@ class CentroDeReciclaje(LugarDeReciclado):
     verificado = models.BooleanField(default=False)
 
     def __str__(self):
-        return "Cooperativa " + self.nombre
+        return "Cooperativa " + self.nombre + " Usuario: " + self.usuario.username
 
 class PuntoDeAcopio(LugarDeReciclado):
     centro = models.ForeignKey('CentroDeReciclaje',related_name='puntos',on_delete=models.CASCADE,)
