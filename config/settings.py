0ec3f712b20a51021b929a82ca55dff4a021fe07
Federico Sironi <sironifede@gmail.com>
2021-11-17 23:19:54 -0300

    changes in settings

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 
 import os
-import json
-with open('/etc/config.json') as config_file:
-    config = json.load(config_file)
-config = config["ozonizers"]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
